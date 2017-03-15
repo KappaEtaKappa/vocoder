b59e8de83faededd4d82f1a191d92edf4dfe2b11
Andrew Zaruba <A.B.Zaruba@gmail.com>
2017-03-14 22:27:35 -0500

    real java project now!

--- Vocoder/src/audio/input/Listener.java
+++
@@ -1,5 +0,0 @@
-package audio.input;
-
-public class Listener {
-
-}
---
+++ src/main/Vocoder.java
@@ -0,0 +1,12 @@
+package main;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+
+public class Vocoder {
+
+	public static void main(String[] args) {
+		
+		AudioFormat af = new AudioFormat(2000.0f, 16, 1, true, true);
+	}
+}
